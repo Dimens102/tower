0f56d594c389fa4727063f097805804f23cc7c9d
Dimens102 <gekkearris@hotmail.com>
2026-06-28 19:04:30 +0200

    Tower v0.3.0 - Main uses command parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-#include <string>
-#include "version.h"
 
-void print_usage() {
+#include "version.h"
+#include "core/command.h"
+
+void print_usage()
+{
     std::cout
         << "Tower Home Automation Engine\n"
         << "Version " << TOWER_VERSION << "\n\n"
@@ -15,45 +17,45 @@ void print_usage() {
         << "  tower config\n";
 }
 
-int main(int argc, char* argv[]) {
-    if (argc == 1) {
+int main(int argc, char* argv[])
+{
+    if (argc == 1)
+    {
         print_usage();
         return 0;
     }
 
-    std::string command = argv[1];
+    switch (parseCommand(argv[1]))
+    {
+        case Command::Version:
+            std::cout << "Tower " << TOWER_VERSION << "\n";
+            break;
 
-    if (command == "version") {
-        std::cout << "Tower " << TOWER_VERSION << "\n";
-        return 0;
+        case Command::Receive:
+            std::cout << "Tower receive mode\n";
+            break;
+
+        case Command::Send:
+            std::cout << "Tower send mode\n";
+            break;
+
+        case Command::Learn:
+            std::cout << "Tower learn mode\n";
+            break;
+
+        case Command::Replay:
+            std::cout << "Tower replay mode\n";
+            break;
+
+        case Command::Config:
+            std::cout << "Tower config mode\n";
+            break;
+
+        default:
+            std::cerr << "Unknown command\n\n";
+            print_usage();
+            return 1;
     }
 
-    if (command == "receive") {
-        std::cout << "Tower receive mode\n";
-        return 0;
-    }
-
-    if (command == "send") {
-        std::cout << "Tower send mode\n";
-        return 0;
-    }
-
-    if (command == "learn") {
-        std::cout << "Tower learn mode\n";
-        return 0;
-    }
-
-    if (command == "replay") {
-        std::cout << "Tower replay mode\n";
-        return 0;
-    }
-
-    if (command == "config") {
-        std::cout << "Tower config mode\n";
-        return 0;
-    }
-
-    std::cerr << "Unknown command: " << command << "\n\n";
-    print_usage();
-    return 1;
+    return 0;
 }
