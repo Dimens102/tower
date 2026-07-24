3825f7ac171b3c34ebd24b83ea5756c65f01c187
Dimens102 <297675948+Dimens102@users.noreply.github.com>
2026-07-24 23:14:28 +0200

    Add timers to timer manager

--- include/service/TimerManager.h
+++ include/service/TimerManager.h
@@ -1,12 +1,21 @@
 #pragma once
 
+#include "service/Callback.h"
 #include "service/Timer.h"
 
+#include <chrono>
 #include <vector>
 
 class TimerManager
 {
 public:
+    using Duration = std::chrono::milliseconds;
+
+    void addTimer(
+        Duration delay,
+        bool repeating,
+        Callback callback);
+
     void update();
 
 private:
--- src/service/TimerManager.cpp
+++ src/service/TimerManager.cpp
@@ -1,5 +1,21 @@
 #include "service/TimerManager.h"
 
+#include <utility>
+
+void TimerManager::addTimer(
+    Duration delay,
+    bool repeating,
+    Callback callback)
+{
+    const Timer::TimePoint expiresAt = Timer::Clock::now() + delay;
+
+    timers_.emplace_back(
+        expiresAt,
+        delay,
+        repeating,
+        std::move(callback));
+}
+
 void TimerManager::update()
 {
 }
